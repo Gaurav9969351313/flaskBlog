4b332d98d168084c74fadf0c2e5c309c4d6cb7ec
TALELE GAURAV <TALEGA-CONT@mahindra.com>
2018-11-22 11:10:01 +0530

    error handling added

--- companyblog/__init__.py
+++ companyblog/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from companyblog.core.views import core
+from companyblog.errorpages.handlers import errorpages
 
 app = Flask(__name__)
 
 app.register_blueprint(core)
+app.register_blueprint(errorpages)
